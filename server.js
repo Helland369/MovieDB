f646288533ff5860886c709f2830340826061434
Thomas Helland <thomas.helland97@gmail.com>
2025-05-09 14:36:08 +0200

    Can now get treinfig movie/tv show data from TMDB

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ dotenv.config();
 
 // load directorys
 app.use(express.static('img'));
+app.use(express.static('views'));
 
 // TMDB api
 const url = 'https://api.themoviedb.org/3/configuration';
-const options = {
+const options =
+{
   method: 'GET',
   headers: {
     accept: 'application/json',
@@ -27,8 +29,50 @@ fetch(url, options)
 
 
 // route
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+app.get('/', (req, res) =>
+{
+  res.send('index.html');
+});
+
+app.get('/tmdb/trending', async (req, res) => {
+  const url = 'https://api.themoviedb.org/3/trending/all/day?language=en-US';
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.TMDB}`
+    }
+  };
+
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) throw new Error(`TMDB error: ${response.status}`);
+    
+    const data = await response.json();
+
+    // loop out the items
+    const htmlItems = data.results.map(item =>
+    {
+      const title = item.title || item.name || 'NO TITLE';
+      return `<li>${title}</li>`;
+    }).join('');
+
+    res.send(`
+      <html>
+        <body>
+          <h1>Trending now!</h1>
+          <ul>
+            <li>${htmlItems}</li>
+          <ul>
+        </body
+      </html>
+    `);
+    
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error fetching trending');
+  }
 });
 
 // server
